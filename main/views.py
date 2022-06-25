abf48bdfedeeadbad33756a0f483af9ea3ea6f49
ssssujung <sujung8553@naver.com>
2022-06-25 23:08:11 +0900

    first commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def introduce(request):
+    return render(request, 'main/introduce.html')
